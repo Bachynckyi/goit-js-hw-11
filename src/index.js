9b5abe90a2252844d4d25957fcdef7f0d9012507
Andrew <bachynckyi.andrii@gmail.com>
2022-08-04 00:13:52 +0300

    update_3

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const submitButton = document.querySelector(".submit-button");
 const gallery = document.querySelector(".gallery");
 const loadButton = document.querySelector(".load-more");
 
-submitButton.addEventListener('click', onSearch);
+submitButton.addEventListener('submit', onSearch);
 
 
 async function onSearch(event) {
@@ -20,9 +20,8 @@ async function onSearch(event) {
             return Notify.failure('Sorry, there are no images matching your search query. Please try again.')
         }
         Notify.info(`Hooray! We found ${data.totalHits} images.`)
-        // galleryMarkUp(data.hits);
+        gallery.innerHTML = galleryMarkUp(data.hits);
     }
-
     catch (error) {
         console.log(error);
     }
@@ -30,28 +29,27 @@ async function onSearch(event) {
 
 function galleryMarkUp(serverArray) {
     return serverArray
-        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-            return serverArray
-            `                 
+        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+            return `                 
         <div class="photo-card">
-        <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
         <div class="info">
             <p class="info-item">
-            <b>Likes${likes}</b>
+            <b>Likes</b>${likes}
             </p>
             <p class="info-item">
-            <b>Views${views}</b>
+            <b>Views</b>${views}
             </p>
             <p class="info-item">
-            <b>Comments${comments}</b>
+            <b>Comments</b>${comments}
             </p>
             <p class="info-item">
-            <b>Downloads${downloads}</b>
+            <b>Downloads</b>${downloads}
             </p>
         </div>
         </div>
-        `
-        });
+        `})
+        .join('');
 };
 
 function clearGallery() {
